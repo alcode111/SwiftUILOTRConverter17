fd334e004c202e6e959d2edd969da0ba87ec358b
alcode111 <al.code.111@gmail.com>
2024-04-01 21:07:48 +0200

    Initiate all the necessary instances of ExchangeRate

--- SwiftUILOTRConverter17/ExchangeInfo.swift
+++ SwiftUILOTRConverter17/ExchangeInfo.swift
@@ -24,7 +24,23 @@ struct ExchangeInfo: View {
                     .font(.title2)
                     .padding()
                 
-//                ExchangeRate()
+                ExchangeRate(leftImage: .goldpiece,
+                             text: "1 Gold Piece = 4 Gold Pennies",
+                             rightImage: .goldpenny)
+                
+                ExchangeRate(leftImage: .goldpenny,
+                             text: "1 Gold Penny = 4 Silver Pieces",
+                             rightImage: .silverpiece)
+                
+                ExchangeRate(leftImage: .silverpiece,
+                             text: "1 Silver Piece = 4 Silver Pennies",
+                             rightImage: .silverpenny)
+                
+                ExchangeRate(leftImage: .silverpenny,
+                             text: "1 Silver Penny = 100 Copper Pennies",
+                             rightImage: .copperpenny)
+                
+                
                 
                 Button("Done") {
                     
