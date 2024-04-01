23e301e421dc9c7abd84cc06d9327006b9b2d1e6
alcode111 <al.code.111@gmail.com>
2024-04-01 14:22:54 +0200

    Initial Commit

---
+++ SwiftUILOTRConverter17/SwiftUILOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILOTRConverter17App.swift
+//  SwiftUILOTRConverter17
+//
+//  Created by Ismaïl LP on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
