3d9c6257084d316b8e10c23a6e0a0019ff097650
alcode111 <al.code.111@gmail.com>
2024-04-02 20:11:45 +0200

    Create a Currency enum

---
+++ SwiftUILOTRConverter17/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  SwiftUILOTRConverter17
+//
+//  Created by Ismaïl LP on 02/04/2024.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
